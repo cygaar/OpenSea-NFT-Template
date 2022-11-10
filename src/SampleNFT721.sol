e8d81fe3ebacc5ffa33cdb50487ce2e2a3ed498c
cygaar <tmpftl1@gmail.com>
2022-11-09 18:48:58 -0800

    Add base template

---
+++ src/SampleNFT721.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "operator-filter-registry/src/OperatorFilterer.sol";
+
+contract SampleNFT721 is ERC721, Ownable, OperatorFilterer {
+    constructor() ERC721("SampleNFT", "SAMPLE") OperatorFilterer(address(0), false) {}
+
+    /* Add your minting logic here */
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
+        public
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
---
+++ src/SampleNFT721A.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "operator-filter-registry/src/OperatorFilterer.sol";
+
+contract SampleNFT721 is ERC721A, Ownable, OperatorFilterer {
+    constructor() ERC721A("SampleNFT", "SAMPLE") OperatorFilterer(address(0), false) {}
+
+    /* Add your minting logic here */
+
+    function transferFrom(address from, address to, uint256 tokenId)
+        public
+        payable
+        override
+        onlyAllowedOperator(from)
+    {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId)
+        public
+        payable
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
+        public
+        payable
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
